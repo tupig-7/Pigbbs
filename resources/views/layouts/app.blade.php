13ca07d17149efe0e915cfc3b73c40c7c7845ea0
Everythone <2664006323@qq.com>
2019-04-23 22:57:13 +0800

    修改配置信息

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CSRF Token-->
+    <meta name="csrf-token" content="{{ csrf-token() }}">
+    <title>@yield('title', '小猪论坛') - Laravel 进阶教程</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+
+</body>
+</html>
